8042d761067f71ff8561002c8052d9e3044bf510
LMK <minkyu.lee@namutech.co.kr>
2023-08-28 10:46:04 +0900

    update remove-source

--- tech-edge/linkerd/remove_resource_nks.sh
+++ tech-edge/linkerd/remove_resource_nks.sh
@@ -8,10 +8,16 @@ kubectl delete -f helloworld-v2.yaml  --context=ctx-2
 
 kubectl delete -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.0/deploy/static/provider/cloud/deploy.yaml --context=ctx-2
 
-kubectl delete ns linkerd-multicluster
+kubectl delete ns linkerd-multicluster --context=ctx-2
 
-kubectl delete ns linkerd-viz
+kubectl delete ns linkerd-viz --context=ctx-2
 
-kubectl delete ns linkerd
+kubectl delete ns linkerd --context=ctx-2
 
-kubectl delete ns ingress-nginx
+kubectl delete ns ingress-nginx --context=ctx-2
+
+kubectl delete ns linkerd-multicluster --context=ctx-1
+
+kubectl delete ns linkerd-viz --context=ctx-1
+
+kubectl delete ns linkerd --context=ctx-1
